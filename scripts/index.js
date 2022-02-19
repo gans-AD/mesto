f1063b0ede54f64330c5bbe6312777fe56c9e19a
Anton Gusev <gans-AD@yandex.ru>
2022-02-19 08:48:06 +0300

    feat: сделать places адаптивным

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,37 @@
-console.log("Привет мир");
+const PopupElement = document.querySelector('.popup'); // popup редактирования профиля
+const popupOpenButtonElement = document.querySelector('.profile__edit-button'); // кнопка редактирования
+const popupCloseButtonElement = document.querySelector('.edit-profile__cls-btn'); //кнопка закрытия окна редактировани
+const popupSaveButtonElemen = document.querySelector('.edit-profile__save-btn'); // кнопка сохранить
+const editPopupNameElement = document.querySelector('.edit__field_name'); //имя пользователя в попапе
+const editPopupActivity = document.querySelector('.edit__field_activity'); //род занятий пользователя в попапе
+const profileName = document.querySelector('.profile__name'); //имя пользователя в профиле на странице
+const profileActivity = document.querySelector('.profile__activity'); //род занятий пользователя в профиле на странице
 
-const editPopupElement = document.querySelector(".edit-popup");
-const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-const popupCloseButtonElement = document.querySelector(".edit-form__cls-btn");
-
-//функция открывания окошка
+//открывание окошка редактирования профиля
 const openPopup = function () {
-  editPopupElement.classList.remove("edit-popup_hidden");
+  PopupElement.classList.add('popup_opened');
+  fillPopup();
 };
 
-//функция закрывания окошка
+//закрывание окошка редактирования профиля
 const closePopup = function () {
-  editPopupElement.classList.add("edit-popup_hidden");
+  PopupElement.classList.remove('popup_opened');
+};
+
+//заполнение popup данны
+const fillPopup = function () {
+  editPopupNameElement.value = profileName.innerText;
+  editPopupActivity.value = profileActivity.innerText;
+};
+
+//кнопка сохранить
+const save = function () {
+  profileName.innerText = editPopupNameElement.value;
+  profileActivity.innerText = editPopupActivity.value;
+  closePopup();
 };
 
 //обработчики событий
-popupOpenButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupSaveButtonElemen.addEventListener('click', save);
