50e3770fa8c77480287d63acd4d296e4803c4118
Anton Gusev <gans-AD@yandex.ru>
2022-03-08 12:31:03 +0300

    fix: исправит некорректное отображение карточек, когда их менее 3шт

--- scripts/index.js
+++ scripts/index.js
@@ -24,8 +24,8 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    name: "Рускеала",
+    link: "https://images.unsplash.com/photo-1548288242-d454d4648b55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
   },
   {
     name: "Иваново",
