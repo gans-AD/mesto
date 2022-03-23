3a19539ca789636348c15d5fab42573d1d567e6a
Anton Gusev <gans-AD@yandex.ru>
2022-03-24 00:11:28 +0300

    feat: добавить валидацию

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,48 @@
-const formProfile = documents.forms.profile;
-const formProfileError = formProfile.querySelector(`.${formProfile.id}-error`);
+//показываем сообщение об ошибках
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`); //ищем блок ошибки по имени input
 
-function showError (input, errorMessage)  {
-  input.classList.add("form__field_type_error");
-  formProfileError.textContent = errorMessage;
+  errorElement.textContent = errorMessage;
+  inputElement.classList.add("form__input_errored");
+  errorElement.classList.add("form__input-error_active");
 };
 
-function hideError (input)  {
-  input.classList.remove("form__field_type_error");
+//скрывает сообщение об ошибках
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`); //ищем блок ошибки по имени input
+
+  errorElement.textContent = "";
+  inputElement.classList.remove("form__input_errored");
+  errorElement.classList.remove("form__input-error_active");
+
 };
 
+//проверяем валидность формы и показываем/скрываем сообщение об ошибках
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+//навешивание валидаций на все input-ы
+function setInputValidations(formElement) {
+  const inputList = formElement.querySelectorAll(".form__input");
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+//валидация всех форм
+function enableValidation() {
+  const formList = document.querySelectorAll(".form");
+  formList.forEach((formElement) => {
+    setInputValidations(formElement);
+  });
+};
+
+enableValidation();
